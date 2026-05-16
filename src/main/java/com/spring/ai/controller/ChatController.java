8382dea01328875b9814df65ebac200319900208
Sarthak Mehta <sarthakmehta@Sarthaks-MacBook-Air.local>
2026-05-16 17:54:08 +0530

    Initial commit of my spring ai project

---
+++ src/main/java/com/spring/ai/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.spring.ai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClient) {
+        this.chatClient = chatClient.build();
+    }
+
+    @GetMapping("/chat")
+    public ResponseEntity<String> chat(@RequestParam(value = "q") String query) {
+        var content = this.chatClient.prompt(query).call().content();
+        return ResponseEntity.ok(content);
+    }
+}
